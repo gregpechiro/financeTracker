37876368a81ab731014ef91f7678c22476528694
therealscout <calebcagno@gmail.com>
2017-01-20 16:04:03 -0500

    started the project

--- main.go
+++ main.go
@@ -1,1 +1,33 @@
 package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/cagnosolutions/adb"
+	"github.com/cagnosolutions/web"
+)
+
+var tmpl *web.TmplCache
+var mux *web.Mux
+var db *adb.DB = adb.NewDB()
+
+func init() {
+	db.AddStore("user")
+	db.AddStore("transaction")
+	db.AddStore("category")
+
+	web.SESSDUR = 15 * time.Minute
+	mux = web.NewMux()
+
+	mux.AddRoutes()
+
+	tmpl = web.NewTmplCache()
+}
+
+func main() {
+	fmt.Println("--------------------did you register all your routes you dumbass")
+	log.Fatal(http.ListenAndServe(":8080", mux))
+}
--- model.go
+++ model.go
@@ -1,1 +1,32 @@
 package main
+
+type User struct {
+	Id          string `json:"id"`
+	Email       string `json:"email,omitempty" auth:"username"`
+	Password    string `json:"password,omitempty" auth:"password"`
+	Active      bool   `json:"active" auth:"active"`
+	Role        string `json:"role,omitempty"`
+	Created     int64  `json:"created,omitempty"`
+	LastSeen    int64  `json:"lastSeen,omitempty"`
+	Phone       string `json:"phone,omitempty"`
+	AccountId   string `json:"accountId,omitempty"`
+	Primary     bool   `json:"primary"`
+	RecoveryPin int    `json:"recoveryPin,omitempty"`
+}
+
+type Transaction struct {
+	Id          string `json:"id"`
+	UserId      string `json:"userId"`
+	Title       string `json:"title,omitempty"`
+	Description string `json:"description,omitempty"`
+	Category    string `json:"category,omitempty"`
+	Amount      int    `json:"amoun,omitemptyt"`
+	Date        int64  `json:"date,omitempty"`
+	AccountId   string `json:"accountId,omitempty"`
+}
+
+type Category struct {
+	Id        string `json:"id"`
+	AccountId string `json:"accountId"`
+	Title     string `json:"title,omitemtpy"`
+}
