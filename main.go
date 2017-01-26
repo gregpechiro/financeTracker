27835a903b0acfda3f8c2697751b9bdbd352f335
therealscout <calebcagno@gmail.com>
2017-01-26 17:05:25 -0500

    added registered and updated login

--- main.go
+++ main.go
@@ -22,7 +22,7 @@ func init() {
 	web.SESSDUR = 15 * time.Minute
 	mux = web.NewMux()
 
-	mux.AddRoutes(home, login, logout, loginPost)
+	mux.AddRoutes(home, login, logout, loginPost, register)
 	mux.AddSecureRoutes(USER, account)
 
 	tmpl = web.NewTmplCache()
@@ -44,9 +44,14 @@ var login = web.Route{"GET", "/login", func(w http.ResponseWriter, r *http.Reque
 }}
 
 var loginPost = web.Route{"POST", "/login", func(w http.ResponseWriter, r *http.Request) {
-	email, pass := r.FormValue("email"), r.FormValue("password")
 	var user User
-	if !db.Auth("user", email, pass, &user) {
+	r.ParseForm()
+	if errs, ok := web.FormToStruct(&user, r.Form, "login"); !ok {
+		web.SetFormErrors(w, errs)
+		web.SetErrorRedirect(w, r, "/login", "Error Loging In")
+		return
+	}
+	if !db.Auth("user", user.Email, user.Password, &user) {
 		web.SetErrorRedirect(w, r, "/login", "Incorrect email or password")
 		return
 	}
@@ -55,7 +60,41 @@ var loginPost = web.Route{"POST", "/login", func(w http.ResponseWriter, r *http.
 	sess.PutId(w, user.Id)
 	sess["email"] = user.Email
 	web.PutMultiSess(w, r, sess)
+	user.LastSeen = time.Now().Unix()
+	db.Set("user", user.Id, user)
+
 	web.SetSuccessRedirect(w, r, "/account", "Welcome "+user.FirstName)
+	return
+
+}}
+
+var register = web.Route{"POST", "/register", func(w http.ResponseWriter, r *http.Request) {
+	var user User
+	r.ParseForm()
+	if errs, ok := web.FormToStruct(&user, r.Form, "register"); !ok {
+		web.SetFormErrors(w, errs)
+		web.SetErrorRedirect(w, r, "/login", "Error Registering")
+		return
+	}
+
+	var users []User
+	db.TestQuery("user", &users, adb.Eq("email", user.Email), adb.Ne("id", `"`+user.Id+`"`))
+	if len(users) > 0 {
+		web.SetErrorRedirect(w, r, "/login", "Error Registering, Email already exists")
+		return
+	}
+
+	user.Id = genId()
+	user.Active = true
+	user.Role = "USER"
+	user.Created = time.Now().Unix()
+	user.Primary = true
+
+	if !db.Add("user", user.Id, user) {
+		web.SetErrorRedirect(w, r, "/login", "Error Registering, Please try again")
+		return
+	}
+	web.SetSuccessRedirect(w, r, "/login", "Registered, Please Login")
 	return
 
 }}
