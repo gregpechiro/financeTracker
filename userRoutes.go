326c7cffdfb121b02fe61699a3f644e28f16fcc6
therealscout <calebcagno@gmail.com>
2017-02-01 17:31:24 -0500

    fixed budget controller

--- userRoutes.go
+++ userRoutes.go
@@ -42,20 +42,20 @@ var budget = web.Route{"GET", "/budget", func(w http.ResponseWriter, r *http.Req
 	id := web.GetId(r)
 	var user User
 
-	//gets user and double checks that the user exists still
+	// gets user and double checks that the user exists still
 	if !db.Get("user", id, &user) {
 		web.Logout(w)
 		web.SetErrorRedirect(w, r, "/login", "Error retrieving user")
 		return
 	}
 
-	//gets all transactions for an account
+	// gets all budgetGroups for an account
 	var budgetGroups []BudgetGroup
-	db.TestQuery("budgetGroups", &budgetGroups, adb.Eq("accountId", `"`+user.AccountId+`"`))
+	db.TestQuery("budgetGroup", &budgetGroups, adb.Eq("accountId", `"`+user.AccountId+`"`))
 
-	//gets all categories for an account
+	// gets all budgetItems for an account
 	var budgetItems []BudgetItem
-	db.TestQuery("budgetItems", &budgetItems, adb.Eq("accountId", `"`+user.AccountId+`"`))
+	db.TestQuery("budgetItem", &budgetItems, adb.Eq("accountId", `"`+user.AccountId+`"`))
 
 	tmpl.Render(w, r, "budget.tmpl", web.Model{
 		"budgetGroups": budgetGroups,
@@ -71,14 +71,14 @@ var transactionSave = web.Route{"POST", "/transaction", func(w http.ResponseWrit
 	id := web.GetId(r)
 	var user User
 
-	//gets user and double checks that the user exists still
+	// gets user and double checks that the user exists still
 	if !db.Get("user", id, &user) {
 		web.Logout(w)
 		web.SetErrorRedirect(w, r, "/login", "Error retrieving user")
 		return
 	}
 
-	//parses form and throws it into a variable
+	// parses form and throws it into a variable
 	var transaction Transaction
 	r.ParseForm()
 	if errs, ok := web.FormToStruct(&transaction, r.Form, "transaction"); !ok {
@@ -86,13 +86,13 @@ var transactionSave = web.Route{"POST", "/transaction", func(w http.ResponseWrit
 		web.SetErrorRedirect(w, r, "/account", "Error saving transaction")
 		return
 	}
-	//assign non parsed fields
+	// assign non parsed fields
 	transaction.Id = genId()
 	transaction.UserId = user.Id
 	transaction.AccountId = user.AccountId
 	transaction.Date = time.Now().Unix()
 
-	//save to db with err check
+	// save to db with err check
 	if !db.Add("transaction", transaction.Id, transaction) {
 		web.SetErrorRedirect(w, r, "/account", "Error saving transcation")
 		return
@@ -106,14 +106,14 @@ var budgetItemSave = web.Route{"POST", "/budgetItem", func(w http.ResponseWriter
 	id := web.GetId(r)
 	var user User
 
-	//gets user and double checks that the user exists still
+	// gets user and double checks that the user exists still
 	if !db.Get("user", id, &user) {
 		web.Logout(w)
 		web.SetErrorRedirect(w, r, "/login", "Error retrieving user")
 		return
 	}
 
-	//parses form and throws it into a variable
+	// parses form and throws it into a variable
 	var budgetItem BudgetItem
 	r.ParseForm()
 	if errs, ok := web.FormToStruct(&budgetItem, r.Form, "budgetItem"); !ok {
@@ -121,11 +121,11 @@ var budgetItemSave = web.Route{"POST", "/budgetItem", func(w http.ResponseWriter
 		web.SetErrorRedirect(w, r, "/budget", "Error saving budget item")
 		return
 	}
-	//assign non parsed fields
+	// assign non parsed fields
 	budgetItem.Id = genId()
 	budgetItem.AccountId = user.AccountId
 
-	//save to db with err check
+	// save to db with err check
 	if !db.Add("budgetItem", budgetItem.Id, budgetItem) {
 		web.SetErrorRedirect(w, r, "/budget", "Error saving budget item")
 		return
@@ -139,14 +139,14 @@ var budgetGroupSave = web.Route{"POST", "/budgetGroup", func(w http.ResponseWrit
 	id := web.GetId(r)
 	var user User
 
-	//gets user and double checks that the user exists still
+	// gets user and double checks that the user exists still
 	if !db.Get("user", id, &user) {
 		web.Logout(w)
 		web.SetErrorRedirect(w, r, "/login", "Error retrieving user")
 		return
 	}
 
-	//parses form and throws it into a variable
+	// parses form and throws it into a variable
 	var budgetGroup BudgetGroup
 	r.ParseForm()
 	if errs, ok := web.FormToStruct(&budgetGroup, r.Form, "budgetGroup"); !ok {
@@ -154,11 +154,11 @@ var budgetGroupSave = web.Route{"POST", "/budgetGroup", func(w http.ResponseWrit
 		web.SetErrorRedirect(w, r, "/budget", "Error saving budget group")
 		return
 	}
-	//assign non parsed fields
+	// assign non parsed fields
 	budgetGroup.Id = genId()
 	budgetGroup.AccountId = user.AccountId
 
-	//save to db with err check
+	// save to db with err check
 	if !db.Add("budgetGroup", budgetGroup.Id, budgetGroup) {
 		web.SetErrorRedirect(w, r, "/budget", "Error saving budget group")
 		return
